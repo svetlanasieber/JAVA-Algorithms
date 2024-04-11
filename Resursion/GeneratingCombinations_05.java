dbe7b7d19acf474477e9cd31561a90a86ae08784
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-04-11 16:43:04 +0200

    Update GeneratingCombinations_05.java

--- Resursion/GeneratingCombinations_05.java
+++ Resursion/GeneratingCombinations_05.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class p05_GeneratingCombinations {
+public class GeneratingCombinations_05 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
