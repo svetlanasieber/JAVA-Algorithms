05c5d7cb75585d0acae932f15a947a4bc63bd66f
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-04-11 16:44:23 +0200

    Update RecursiveDrawing_03.java

--- Resursion/RecursiveDrawing_03.java
+++ Resursion/RecursiveDrawing_03.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class p03_RecursiveDrawing {
+public class RecursiveDrawing_03 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
