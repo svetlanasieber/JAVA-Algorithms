2fa035c6f5e70b925c26de775de66c817bbfa8f9
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-04-11 16:43:24 +0200

    Update GeneratingVectors_04.java

--- Resursion/GeneratingVectors_04.java
+++ Resursion/GeneratingVectors_04.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class p04_GeneratingVectors {
+public class GeneratingVectors_04 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
